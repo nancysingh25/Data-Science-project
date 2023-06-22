4597adcd62371076a85f1ed548aca5bc2917232e
nancysingh25 <57943092+nancysingh25@users.noreply.github.com>
2023-06-22 19:11:29 +0200

    Update Exploratory_dataAnalysis.R

--- Exploratory_dataAnalysis.R
+++ Exploratory_dataAnalysis.R
@@ -42,3 +42,42 @@ mean(heights$ht_cm, na.rm=TRUE)
 # new datframe contaimig only female observation
 heights2 <-data.frame(heights %>% filter(heights$sex =="Female"))
  mean(heights2$ht_cm)
+
+#sumarry of height variable dataset
+summary(heights$height)
+
+#find the percentile of height$height
+p<-seq(0.01, 0.99, 0.01)
+percentiles<- quantile(heights$height, p)
+percentiles
+
+#to confirm the 25th and 75th percentile matches the 1st and 3rd quartile
+percentiles[names(percentiles) == "25%"]
+percentiles[names(percentiles) == "75%"]
+
+#plotting male height data from the dataset
+p <- heights %>%
+  filter(sex == "Male") %>%
+  ggplot(aes(x = height))
+
+p + geom_histogram(binwidth = 1, fill = "blue", col = "black") +
+  xlab("Male heights in inches") +
+  ggtitle("Histogram")
+
+#to create a smooth density plot the male height data
+p + geom_density()
+p + geom_density(fill = "blue")
+
+#QQ  QQ-plot against a normal distribution with same mean/sd as data
+params <- heights %>%
+  filter(sex == "Male") %>%
+  summarize(mean = mean(height), sd = sd(height))
+p + geom_qq(dparams = params) +
+  geom_abline()
+
+# QQ-plot of scaled data against the standard normal distribution
+heights %>%
+  ggplot(aes(sample = scale(height))) +
+  geom_qq() +
+  geom_abline()
+
